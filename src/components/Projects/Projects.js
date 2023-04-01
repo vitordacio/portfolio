7ca50895745c8384f5195d2051d7a7cb8ac7e814
Vitor Dacio <vitordacio@hotmail.com>
2023-03-31 22:05:41 -0300

    improve projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,24 +2,51 @@ import './styles.css'
 
 import { useEffect, useState } from 'react'
 
-import Project from '../Project/Project'
+import { GrCaretNext, GrCaretPrevious } from 'react-icons/gr'
+
 import data from '../../data/projects'
+import Project from '../Project/Project'
 
 
 const Projects = () => {
-    const [projects, setProjects] = useState([])
+    const [projects, setProjects] = useState([...data])
+    const [page, setPage] = useState(1)
+    const [currentProjects, setCurrentProjects] = useState([])
+
+    const handlePrevius = () => {
+        if (page === 1) {
+            return
+        }
+        setPage(page - 1)
+    }
+
+    const handleNext = () => {
+        if (page * 6 >= projects.length) {
+            return
+        }
+        setPage(page + 1)
+    }
 
     useEffect(() => {
-        setProjects(data)
-    }, [])
+        console.log(page, projects.length)
+        const first = (page * 6) - 6
+        const last = 6 * page
+        setCurrentProjects(projects.slice(first, last))
+    }, [page])
 
     return (
         <div id='projects' className="projects container">
             <h2>Projetos</h2>
             <p>Aqui estão alguns dos projetos que já desenvolvi:</p>
-            <div className="projectsContainer">
-                {projects.map((project) => <Project key={project.id} item={project} />)}
+            <div className='actions'>
+                <GrCaretPrevious onClick={handlePrevius} />
+                <span>{page}</span>
+                <GrCaretNext onClick={handleNext} />
             </div>
+            <div className="projectsContainer">
+                {currentProjects.map((project) => <Project key={project.id} item={project} />)}
+            </div>
+
         </div>
     )
 }
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,60 +1,95 @@
 const projects = [
     {
         id: 1,
-        image: 'https://i.ibb.co/N29rrF3/image.png',
-        name: 'CubosFlix (Desafio Front-End M02)',
-        linkCode: 'https://github.com/vitordacio/desafio-frontend-m02',
-        linkOn: 'https://desafio-frontend-m02.vercel.app/'
-    },
-    {
-        id: 2,
+        name: 'Front-End: Lotes do Vale',
         image: 'https://i.ibb.co/8dm9493/image.png',
-        name: 'Projeto destinado a corretor de imóveis local para venda de lotes',
         linkCode: 'https://github.com/vitordacio/modelo_lotesdovale',
         linkOn: 'https://modelo-lotesdovale.vercel.app/'
     },
     {
+        id: 2,
+        name: 'Full-Stack: Dindin (Desafio M03)',
+        image: 'https://camo.githubusercontent.com/b307613707fd3e911fd85759fbca812305ec084c018354ae1f9465674d1745ac/68747470733a2f2f692e696d6775722e636f6d2f76766e6c756a362e706e67',
+        linkCode: 'https://github.com/vitordacio/dindin-frontend',
+        linkOn: 'https://dindin-frontend.vercel.app/'
+    },
+    {
         id: 3,
-        image: 'https://i.ibb.co/3v7t7x8/image.png',
-        name: 'Exercícios de Rotas (react-router-dom)',
-        linkCode: 'https://github.com/vitordacio/exercicio-rotas-react-router-dom',
-        linkOn: 'https://exercicio-rotas-react-router-dom.vercel.app/'
+        name: 'Back-End: Dindin (Desafio M03)',
+        image: 'https://www.astera.com/wp-content/uploads/2020/01/rest.png',
+        linkCode: 'https://github.com/vitordacio/dindin-backend-api',
+        linkOn: 'https://nice-jade-coypu-gown.cyclic.app/'
     },
     {
         id: 4,
+        name: 'Front-End: CubosFlix (Desafio M02)',
+        image: 'https://i.ibb.co/N29rrF3/image.png',
+        linkCode: 'https://github.com/vitordacio/desafio-frontend-m02',
+        linkOn: 'https://desafio-frontend-m02.vercel.app/'
+    },
+    {
+        id: 5,
+        name: 'Back-End: Sistema para bancos (Desafio  M02)',
         image: 'https://www.astera.com/wp-content/uploads/2020/01/rest.png',
-        name: 'Sistema para bancos (Desafio Back-End M02)',
         linkCode: 'https://github.com/vitordacio/desafio-backend-m02',
         linkOn: '',
     },
     {
-        id: 5,
-        image: 'https://www.astera.com/wp-content/uploads/2020/01/rest.png',
-        name: 'Api para sistemas de Biblioteca (conexão-nodejs-postgresql)',
-        linkCode: 'https://github.com/vitordacio/exercicio-conexao-nodejs-protegresql',
-        linkOn: ''
-    },
-    {
         id: 6,
-        image: 'https://www.astera.com/wp-content/uploads/2020/01/rest.png',
-        name: 'Api com autenticação de login (criptografia e token JWT)',
-        linkCode: 'https://github.com/vitordacio/exercicio-autenticacao-criptografia',
-        linkOn: ''
-    },
-    {
-        id: 7,
+        name: 'Front-End: Portfólio (Desafio M01)',
         image: 'https://camo.githubusercontent.com/df160220fd25d3ed0e8fc2ebbaf31c8ea70b42799bf36a389d2e22954f1c60ab/68747470733a2f2f692e696d6775722e636f6d2f746a594f3652362e706e67',
-        name: 'Modelo Portfólio  (Desafio Front-End M01)',
         linkCode: 'https://github.com/vitordacio/desafio-frontend-m01',
         linkOn: 'https://desafio-frontend-m01.vercel.app/'
     },
     {
+        id: 7,
+        name: 'Back-End: Marketplace API',
+        image: 'https://www.astera.com/wp-content/uploads/2020/01/rest.png',
+        linkCode: 'https://github.com/vitordacio/api-marketplace',
+        linkOn: 'https://nice-ruby-gosling-shoe.cyclic.app/'
+    },
+    {
         id: 8,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png',
-        name: 'Em breve',
-        linkCode: '',
+        name: 'Back-End: Biblioteca (conexão-nodejs-postgresql)',
+        image: 'https://www.astera.com/wp-content/uploads/2020/01/rest.png',
+        linkCode: 'https://github.com/vitordacio/exercicio-conexao-nodejs-protegresql',
         linkOn: ''
     },
+    {
+        id: 9,
+        name: 'Front-End: MarketCubos (Design System - MUI)',
+        image: 'https://i.ibb.co/N6dgFSr/c1a9c3a3-9784-4708-a206-0273f2444619.png',
+        linkCode: 'https://github.com/vitordacio/design-system-mui',
+        linkOn: 'https://design-system-mui.vercel.app/'
+    },
+    {
+        id: 10,
+        name: 'Back-End: Login (criptografia e token JWT)',
+        image: 'https://www.astera.com/wp-content/uploads/2020/01/rest.png',
+        linkCode: 'https://github.com/vitordacio/exercicio-autenticacao-criptografia',
+        linkOn: ''
+    },
+    {
+        id: 11,
+        name: 'Front-End: Exercícios de Rotas (react-router-dom)',
+        image: 'https://i.ibb.co/3v7t7x8/image.png',
+        linkCode: 'https://github.com/vitordacio/exercicio-rotas-react-router-dom',
+        linkOn: 'https://exercicio-rotas-react-router-dom.vercel.app/'
+    },
+    {
+        id: 12,
+        name: 'Back-End: Envio de E-mail',
+        image: 'https://www.astera.com/wp-content/uploads/2020/01/rest.png',
+        linkCode: 'https://github.com/vitordacio/send-email-api',
+        linkOn: ''
+    },
+    {
+        id: 13,
+        name: 'Em breve',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png',
+        linkCode: '',
+        linkOn: ''
+    }
 ]
 
 export default projects
